0d3a2aab355557b2160b24f0e8c1c773dc57f60a
Uryel Farias <uryel.farias@gmail.com>
2021-06-08 00:37:52 -0300

    ok

---
+++ moduloD/aula_11.js
@@ -0,0 +1,8 @@
+var velocidade = 45.6
+console.log(`A sua velocidade atual é de ${velocidade}.`)
+if (velocidade > 60){
+    console.log('Você está acima da velocidade permitida, logo recerá uma MULTA!.')
+} else {
+    console.log('Você está dentro da velocidade permitida, continue dirigindo com segurança!')
+}
+
