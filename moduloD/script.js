f7009adebceec9a79b026c9c3e6b13533346a5e5
Uryel Farias <uryel.farias@gmail.com>
2021-06-19 07:32:50 -0300

    ExercicioHoraDodia

---
+++ moduloD/script.js
@@ -0,0 +1,21 @@
+
+function carregar(){
+    var mensagem = window.document.getElementById('mensagem')
+    var img = window.document.getElementById('manha')
+    var data = new Date()
+    var hora = data.getHours()
+    //hora = 19
+    var minuto = data.getMinutes()
+    mensagem.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+
+    if (hora >= 0 && hora <= 12){
+        img.src = 'amanhecer.png'
+        document.body.style.background = '#f4c900'
+    } else if ( hora > 12 && hora <= 18) {
+        img.src = 'entardecer.png'
+        document.body.style.background = '#fec3a5'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#252c46'
+    }
+}
